f655520b81b8ba5157a9d5fc1db173e8dea412d2
rnathan <rnathan@pandora.com>
2018-08-17 11:56:48 -0700

    Initial Commit

---
+++ Diffable/Diffable.h
@@ -0,0 +1,19 @@
+//
+//  Diffable.h
+//  Diffable
+//
+//  Created by Roshan Balaji Nindrai Senthilnathan on 8/17/18.
+//  Copyright © 2018 Roshan Balaji Nindrai Senthilnathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Diffable.
+FOUNDATION_EXPORT double DiffableVersionNumber;
+
+//! Project version string for Diffable.
+FOUNDATION_EXPORT const unsigned char DiffableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Diffable/PublicHeader.h>
+
+
